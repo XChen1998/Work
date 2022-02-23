715e067418044592e2ecca7f4bf784b47bd2f37e
Xinghe Chen <xinghechen98@outlook.com>
2022-02-23 13:35:52 +0800

    Working on section XI.

--- Courses/CS61B_2018_Spring_UCB/CS61B_Readings/11. Balanced Trees/RBBST/src/RedBlackBST.java
+++ Courses/CS61B_2018_Spring_UCB/CS61B_Readings/11. Balanced Trees/RBBST/src/RedBlackBST.java
@@ -3,29 +3,29 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
 
     private static final boolean RED = true;
     private static final boolean BLACK = false;
-    private class Node
-    {
+
+    private class Node {
         Key key; // key
         Value val; // associated data
         Node left, right; // subtrees
         int N; // # nodes in this subtree
         boolean color; // color of link from
+
         // parent to this node
-        Node(Key key, Value val, int N, boolean color)
-        {
+        Node(Key key, Value val, int N, boolean color) {
             this.key = key;
             this.val = val;
             this.N = N;
             this.color = color;
         }
     }
-    private boolean isRed(Node x)
-    {
+
+    private boolean isRed(Node x) {
         if (x == null) return false;
         return x.color == RED;
     }
 
-    private Node rotateLeft(Node h){
+    private Node rotateLeft(Node h) {
         Node x = h.right;
         h.right = x.left;
         x.left = h;
@@ -36,20 +36,25 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         return x;
     }
 
-    private Node rotateRight(Node h){
+    private Node rotateRight(Node h) {
         Node x = h.left;
         h.left = x.right;
         x.right = h;
         x.color = h.color;
         h.color = RED;
         h.N = size(h.left) + size(h.right) + 1;
-        x.N = h.N
-    } // See page 434.
+        x.N = h.N;
+        return x;
+    }
 
-    private void flipColors(Node h) // See page 436.
+    private void flipColors(Node h) {
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+    }
 
-    private int size(Node x){
-        if (x == null){
+    private int size(Node x) {
+        if (x == null) {
             return 0;
         }
         return x.N;
@@ -57,19 +62,29 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
 
     public void put(Key key, Value val) { // Search for key. Update value if found; grow table if new.
         root = put(root, key, val);
-        root.color = BLACK;
+        root.color = BLACK; // Always make sure that the root is a black node.
     }
 
     private Node put(Node h, Key key, Value val) {
         if (h == null) // Do standard insert, with red link to parent.
             return new Node(key, val, 1, RED);
         int cmp = key.compareTo(h.key);
-        if (cmp < 0) h.left = put(h.left, key, val);
-        else if (cmp > 0) h.right = put(h.right, key, val);
-        else h.val = val;
-        if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
-        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
-        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+        if (cmp < 0) {
+            h.left = put(h.left, key, val);
+        } else if (cmp > 0) {
+            h.right = put(h.right, key, val);
+        } else {
+            h.val = val;
+        }
+        if (isRed(h.right) && !isRed(h.left)) {
+            h = rotateLeft(h);
+        }
+        if (isRed(h.left) && isRed(h.left.left)) {
+            h = rotateRight(h);
+        }
+        if (isRed(h.left) && isRed(h.right)) {
+            flipColors(h);
+        }
         h.N = size(h.left) + size(h.right) + 1;
         return h;
     }
