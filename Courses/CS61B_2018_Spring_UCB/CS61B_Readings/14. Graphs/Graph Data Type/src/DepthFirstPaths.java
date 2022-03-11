40e3a2398d40a6286ab9c5de8e65d53d5dbaa91d
Xinghe Chen <xinghechen98@outlook.com>
2022-03-11 17:16:02 +0800

    CS61B, depth-first paths test added.

--- Courses/CS61B_2018_Spring_UCB/CS61B_Readings/14. Graphs/Graph Data Type/src/DepthFirstPaths.java
+++ Courses/CS61B_2018_Spring_UCB/CS61B_Readings/14. Graphs/Graph Data Type/src/DepthFirstPaths.java
@@ -34,18 +34,21 @@ public class DepthFirstPaths {
         return path;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Graph G = new Graph(new In(args[0]));
         int s = Integer.parseInt(args[1]);
         DepthFirstPaths search = new DepthFirstPaths(G, s);
-        for (int v = 0; v < G.V(); v++)
-        {
+        for (int v = 0; v < G.V(); v++) {
             StdOut.print(s + " to " + v + ": ");
-            if (search.hasPathTo(v))
-                for (int x : search.pathTo(v))
-                    if (x == s) StdOut.print(x);
-                    else StdOut.print("-" + x);
+            if (search.hasPathTo(v)) {
+                for (int x : search.pathTo(v)) {
+                    if (x == s) {
+                        StdOut.print(x);
+                    } else {
+                        StdOut.print(x + "-");
+                    }
+                }
+            }
             StdOut.println();
         }
     }
