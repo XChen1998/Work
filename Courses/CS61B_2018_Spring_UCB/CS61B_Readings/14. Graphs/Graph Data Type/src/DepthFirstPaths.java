77cf02cb8b46a1348d8794c7333020752045cb27
Xinghe Chen <xinghechen98@outlook.com>
2022-03-11 17:06:03 +0800

    CS61B files reorganised.

--- Courses/CS61B_2018_Spring_UCB/CS61B_Readings/14. Graphs/Graph Data Type/src/DepthFirstPaths.java
+++ Courses/CS61B_2018_Spring_UCB/CS61B_Readings/14. Graphs/Graph Data Type/src/DepthFirstPaths.java
@@ -33,4 +33,20 @@ public class DepthFirstPaths {
         path.push(s);
         return path;
     }
+
+    public static void main(String[] args)
+    {
+        Graph G = new Graph(new In(args[0]));
+        int s = Integer.parseInt(args[1]);
+        DepthFirstPaths search = new DepthFirstPaths(G, s);
+        for (int v = 0; v < G.V(); v++)
+        {
+            StdOut.print(s + " to " + v + ": ");
+            if (search.hasPathTo(v))
+                for (int x : search.pathTo(v))
+                    if (x == s) StdOut.print(x);
+                    else StdOut.print("-" + x);
+            StdOut.println();
+        }
+    }
 }
