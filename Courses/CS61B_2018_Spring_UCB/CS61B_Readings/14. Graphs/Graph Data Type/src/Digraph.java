68a15adc7bfd07f3a93085f11c7d4a1c8a8e661c
Xinghe Chen <xinghechen98@outlook.com>
2022-03-14 13:25:14 +0800

    Update Digraph.java

--- Courses/CS61B_2018_Spring_UCB/CS61B_Readings/14. Graphs/Graph Data Type/src/Digraph.java
+++ Courses/CS61B_2018_Spring_UCB/CS61B_Readings/14. Graphs/Graph Data Type/src/Digraph.java
@@ -1,29 +1,37 @@
 import edu.princeton.cs.algs4.Bag;
 
-public class Digraph
-{
+public class Digraph {
     private final int V;
     private int E;
     private Bag<Integer>[] adj;
-    public Digraph(int V)
-    {
+
+    public Digraph(int V) {
         this.V = V;
         this.E = 0;
         adj = (Bag<Integer>[]) new Bag[V];
         for (int v = 0; v < V; v++)
             adj[v] = new Bag<Integer>();
     }
-    public int V() { return V; }
-    public int E() { return E; }
-    public void addEdge(int v, int w)
-    {
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
         adj[v].add(w);
         E++;
     }
-    public Iterable<Integer> adj(int v)
-    { return adj[v]; }
-    public Digraph reverse()
-    {
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    /*Reverse the digraph.*/
+    public Digraph reverse() {
         Digraph R = new Digraph(V);
         for (int v = 0; v < V; v++)
             for (int w : adj(v))
