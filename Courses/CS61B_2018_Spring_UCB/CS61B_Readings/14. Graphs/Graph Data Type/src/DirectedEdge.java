3f6d90b1bf1dd9329790b38e04d9e701aa657e05
Xinghe Chen <xinghechen98@outlook.com>
2022-03-15 23:49:46 +0800

    CS61B EdgeWeightedDigraph.class added.

---
+++ Courses/CS61B_2018_Spring_UCB/CS61B_Readings/14. Graphs/Graph Data Type/src/DirectedEdge.java
@@ -0,0 +1,27 @@
+public class DirectedEdge {
+    private final int v; // edge source
+    private final int w; // edge target
+    private final double weight; // edge weight
+
+    public DirectedEdge(int v, int w, double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    public String toString() {
+        return String.format("%d->%d %.2f", v, w, weight);
+    }
+}
