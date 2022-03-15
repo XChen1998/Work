a347f5f0d3da5c423bcc05849cbb5d3455ac8c8d
Xinghe Chen <xinghechen98@outlook.com>
2022-03-16 01:34:03 +0800

    CS61B methods related to Dijkstra algorithm added.

---
+++ Courses/CS61B_2018_Spring_UCB/CS61B_Readings/14. Graphs/Graph Data Type/src/DijkstraSP.java
@@ -0,0 +1,65 @@
+import edu.princeton.cs.algs4.IndexMinPQ;
+import edu.princeton.cs.algs4.Stack;
+
+public class DijkstraSP {
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+    private IndexMinPQ<Double> pq;
+
+    public DijkstraSP(EdgeWeightedDigraph G, int s) {
+        edgeTo = new DirectedEdge[G.V()];
+        distTo = new double[G.V()];
+        pq = new IndexMinPQ<Double>(G.V());
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY; //XC: set all distance to positive infinity.
+        distTo[s] = 0.0;
+        pq.insert(s, 0.0);
+        while (!pq.isEmpty())
+            relax(G, pq.delMin());
+    }
+
+    /*XC: This is teh key of Dijkstra algorithm.*/
+    private void relax(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e : G.adj(v)) {
+            int w = e.to();
+            if (distTo[w] > distTo[v] + e.weight()) {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+                if (pq.contains(w)) pq.changeKey(w, distTo[w]);
+                else pq.insert(w, distTo[w]);
+            }
+        }
+    }
+
+    public double distTo(int v) {
+        return distTo[v];
+    }
+
+    public boolean hasPathTo(int v) {
+        return distTo[v] < Double.POSITIVE_INFINITY;
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()])
+            path.push(e);
+        return path;
+    }
+
+    public static void main(String[] args) {
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(new In(args[0]));
+        int s = Integer.parseInt(args[1]);
+        DijkstraSP search = new DijkstraSP(G, s);
+        for (int v = 0; v < G.V(); v++) {
+            StdOut.println(s + " to " + v + ": ");
+            if (search.hasPathTo(v)) {
+                for (DirectedEdge x : search.pathTo(v)) {
+                    StdOut.println(x + "  ");
+                }
+            }
+        }
+        StdOut.println();
+    }
+}
+
