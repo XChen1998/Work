3c1cef3e0ced6db2f9d0d0ef9310c8e7a480ff39
Xinghe Chen <xinghechen98@outlook.com>
2022-03-25 13:23:01 +0800

    CS61B, lab 11, working on MazeCycles.class.

--- Courses/CS61B_2018_Spring_UCB/CS61B_Assignments/lab11/lab11/graphs/MazeCycles.java
+++ Courses/CS61B_2018_Spring_UCB/CS61B_Assignments/lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -9,19 +11,45 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-    boolean hasCircle;
+    private int[] circleList;
 
     public MazeCycles(Maze m) {
         super(m);
-        hasCircle = false;
+        circleList = new int[m.V()];
+        maze = m;
+        circleList[0] = 0;
     }
 
     @Override
     public void solve() {
-        
+        dfsCircle(0);
+        announce();
     }
 
 
     // Helper methods go here
+    private void dfsCircle(int v) {
+        marked[v] = true;
+        announce();
+        System.out.println(v);
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                if (w != v){
+                    int next = circleList[w];
+                    int previous = w;
+                    while (next != w){
+                        edgeTo[next] = previous;
+                        previous = next;
+                        next = circleList[previous];
+                    }
+                    return;
+                }
+                circleList[w] = v;
+                dfsCircle(w);
+            }
+        }
+
+
+    }
 }
 
