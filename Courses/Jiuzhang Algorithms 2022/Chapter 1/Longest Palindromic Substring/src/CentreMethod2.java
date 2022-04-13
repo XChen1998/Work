a1b53d3cf610349e2c65ae364a3fed6a6c13208b
Xinghe Chen <90239395+XChen1998@users.noreply.github.com>
2022-04-13 19:03:58 +0800

    JZ Algs 22, Longest Palindromic Substring.
    CentreMethod with optimised coding added.

---
+++ Courses/Jiuzhang Algorithms 2022/Chapter 1/Longest Palindromic Substring/src/CentreMethod2.java
@@ -0,0 +1,36 @@
+public class CentreMethod2 {
+    /**
+     * @param s: input string
+     * @return: a string as the longest palindromic substring
+     */
+    public String longestPalindrome(String s) {
+        // write your code here
+        if (s == null || s == "") {
+            return "";
+        }
+
+        String longestPalindrome = "";
+
+        for (int midPos = 0; midPos < s.length(); midPos++) {
+            //odd case
+            String oddLongestString = isPalindromic(s, midPos, midPos);
+            //even case
+            String evenLongestString = isPalindromic(s, midPos, midPos + 1);
+            String longestStringAtM = oddLongestString.length() > evenLongestString.length() ? oddLongestString : evenLongestString;
+            longestPalindrome = longestPalindrome.length() > longestStringAtM.length() ? longestPalindrome : longestStringAtM;
+        }
+
+        return longestPalindrome;
+
+    }
+    private String isPalindromic(String s, int leftPos, int rightPos){
+        while (leftPos >= 0 && rightPos < s.length()){
+            if (s.charAt(leftPos) != s.charAt(rightPos)){
+                break;
+            }
+            leftPos--;
+            rightPos++;
+        }
+        return s.substring(leftPos + 1, rightPos);
+    }
+}
---
+++ Courses/Jiuzhang Algorithms 2022/Chapter 1/Longest Palindromic Substring/src/TestLPS.java
@@ -0,0 +1,13 @@
+public class TestLPS {
+    public static void main(String[] args) {
+        String[] test = {"abcbe", "b", "bb", "ca", "mata", "fdaslneblkwgbliwqorefdjdlksanczxbvkhfdfdfdfdfdssewdvcxza"};
+        MySolution ms = new MySolution();
+        CentreMethod2 cm2 = new CentreMethod2();
+        for (String s : test){
+            System.out.println("CurrentString: " + s);
+            System.out.println("My solution: " + ms.longestPalindrome(s));
+            System.out.println("CentreMethod2: " + cm2.longestPalindrome(s));
+            System.out.println();
+        }
+    }
+}
